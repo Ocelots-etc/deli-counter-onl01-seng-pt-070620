b13df313edf6504a4d81759f20f088bbd23317fd
Cat Gomez <catherinedgomez@gmail.com>
2020-07-17 12:47:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(array, name)
 
   array.each_with_index do |name, index|
     "Welcome, #{name}. You are number #{index + 1} in line."
-    katz_deli << "#{name}"
+    array << "#{name}"
     # new_array << "#{index + 1}. #{name}"
   end
 end
